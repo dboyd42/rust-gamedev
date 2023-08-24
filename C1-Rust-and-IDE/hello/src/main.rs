9996939cc0a394caa2144312f674b3c80e2f9f50
dboyd42 <bhatmagician@gmail.com>
2023-08-24 10:54:17 -0500

    reboot

--- C1-Rust-and-IDE/hello/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![warn(clippy::all, clippy::pedantic)]
-fn main() {
-    // println!("Hello, world!");
-    let my_list = [ "One", "Two", "Three" ];
-    // for i in 0..3 {
-    for item in &my_list {
-        println!("{}", item);
-    }
-}
